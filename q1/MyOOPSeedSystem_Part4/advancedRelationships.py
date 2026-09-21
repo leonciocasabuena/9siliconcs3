ce45f5007c870ec00af597e7a583bbaf4f60651b
leonciocasabuena <lbcasabuena@brc.pshs.edu.ph>
2026-09-21 13:17:09 +0800

    Update advancedRelationships.py

--- q1/MyOOPSeedSystem_Part4/advancedRelationships.py
+++ q1/MyOOPSeedSystem_Part4/advancedRelationships.py
@@ -1,1 +1,14 @@
+class Instrument:
+  def __init__(self, Color, Type, IsTuned, IsPlayable):
+    self.color = Color
+    self.__type = Type
+    self.IsTuned = IsTuned
+    self.__IsPlayable = IsTuned
 
+
+class Guitar(Instrument):
+  def __init__(self, Color, Type, IsTuned, IsPlayable):
+    super().__init__(MarketPrice, ProductionYear)
+      self.__marketprice = MarketPrice
+      self.__productionyear = ProductionYear
+    
