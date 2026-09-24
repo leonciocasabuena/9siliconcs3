23a54cd4b7dd880e26b1ab231270eb270bc74c89
leonciocasabuena <lbcasabuena@brc.pshs.edu.ph>
2026-09-24 21:24:36 +0800

    Update advancedRelationships.py

--- q1/MyOOPSeedSystem_Part4/advancedRelationships.py
+++ q1/MyOOPSeedSystem_Part4/advancedRelationships.py
@@ -1,5 +1,5 @@
-#INHERITANCE SYSTEM
-class Instrument:
+
+class Instrument: #Creates a class named "Instrument".
   def __init__(self, Color, Type, IsTuned, IsPlayable):
       self.color = Color
       self.__type = Type
@@ -9,26 +9,42 @@ class Instrument:
   def get_color(self):
       return self.color
 
-class Guitar(Instrument):
-  def __init__(self, Color, Type, IsTuned, IsPlayable, MarketPrice, ProductionYear, Strings):
+class guitar_components: #Creates a class "guitar_components" which iterates the components of the child class that will be used for a composition relationship later.
+
+  def __init__(self, String):
+      self.__string = String
+
+  def get_strings(self):
+      return self.__string
+
+class Guitar(Instrument): #Creates a child class named "Guitar" and declares the class "Instrument" as parent class.
+  def __init__(self, Color, Type, IsTuned, IsPlayable, MarketPrice, ProductionYear):
       super().__init__(Color, Type, IsTuned, IsPlayable,)
       self.__marketprice = MarketPrice
       self.__productionyear = ProductionYear
-      self.string = Strings #Creates a composition relationship with an object named "String"
-
+      self.string = guitar_components("6-strings") #Creates a composition relationship with an object inside the class "guitar_components" by calling the getter function from the said class.
+    
   def get_color(self):
       return self.color
-    
-parent_instrument=Instrument("Indigo", "Guitar", "True", "True")  
-guitar=Guitar("Indigo", "Guitar", "True", "True", "5000", "2026", "6 Strings")
 
-print("---TEST 1: INHERITANCE---")
+  #Creates a function that calls the declared attribute that made a composition relationship with an object from the "guitar_components" class
+  def get_Strings(self):
+      return self.string.get_strings()
+
+#Instantiates an object from each parent and child class.
+parent_instrument=Instrument("Indigo", "Guitar", "True", "True")  
+guitar=Guitar("Indigo", "Guitar", "True", "True", "5000", "2026")
+
+#Tests the inheritance system by calling the attributes of the parent class from the child class.
+print("---TEST 1: INHERITANCE RELATIONSHIP---")
 print("Parent attribute:") 
 print(f"Color: {parent_instrument.get_color()}")
 print("")
 print("Child object:")
-print(f"Color: {guitar.get_color()}")
+print("Guitar")
+print(f"\t-Color: {guitar.get_color()}")
 
-
+#Tests the composition system by performing the previously set-up composition system between the "guitar_components" and "Guitar" classes.
 print("")
-print("---TEST 2: COMPOSITION---")
+print("---TEST 2: COMPOSITION RELATIONSHIP---")
+print(f"Guitar contains strings: {guitar.get_Strings()}")
